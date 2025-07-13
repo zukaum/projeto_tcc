4fca31cffd612d946237e09b1d5b89bf424d912b
Udson Zukowski <udsonzukowski@DSA-MDMA02.local>
2025-07-12 22:23:22 -0300

    feat: atualiza endpoint /register para novos campos

--- auth-service/app/models.py
+++ auth-service/app/models.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel, EmailStr
+from datetime import date
+from .schemas import UserRole
 
 class UserIn(BaseModel):
+    nome_completo: str
+    id_documento: str
     email: EmailStr
     password: str
-    role: str
+    role: UserRole
+    data_nascimento: date
 
 class UserOut(BaseModel):
     email: EmailStr
